4ffe0e85bc8f5a0a325501310cf95b1e2ec1c84e
Hadi <sunaryohadi@gmail.com>
2019-07-12 13:34:18 +0700

    Upgrade

--- src/screens/common/NavBar/index.js
+++ src/screens/common/NavBar/index.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export class NavBar extends Component {
   render() {
     return (
-      <div>
-        Nav Bar
-      </div>
+      <header>
+        <div className="navbar">
+          Nav Bar
+        </div>
+        <ul class="sidenav">
+          <li><a href="/manage" class="logo-container">Admin<i class="material-icons left">spa</i></a></li>
+        </ul>
+      </header>
     )
   }
 }
